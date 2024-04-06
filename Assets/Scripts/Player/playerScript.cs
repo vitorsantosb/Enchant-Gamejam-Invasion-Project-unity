f83b8504b3a3643c0352f245c53fa2cabd192077
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-04-06 18:39:49 -0300

    multiplayer

--- Assets/Scripts/Player/playerScript.cs
+++ Assets/Scripts/Player/playerScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
 public class playerScript : MonoBehaviour
 {
@@ -11,28 +12,52 @@ public class playerScript : MonoBehaviour
     private List<PotionEffect> effects;
     private TextMeshPro potionListText;
 
+    public PhotonView view;
+    public GameObject cameraPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         this.potionListText = GetComponent<TextMeshPro>();
         this.effects = new List<PotionEffect>();
         this.body = GetComponent<Rigidbody2D>();
+        view = GetComponent<PhotonView>();
+
+        // Check if the player is local, if it is set a camera, if not disable the camera
+        if (view.IsMine)
+        {
+            GameObject cameraObj = Instantiate(cameraPrefab, transform.position, Quaternion.identity);
+            cameraObj.transform.position = new Vector3(cameraObj.transform.position.x, cameraObj.transform.position.y, -10f);
+            cameraObj.transform.parent = transform;
+        }
+        else
+        {
+            Camera camera = GetComponentInChildren<Camera>();
+            if(camera != null)
+            {
+                camera.gameObject.SetActive(false);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (view.IsMine)
         {
-            AddPotionEffect(new PotionEffect(5, 5000));
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                AddPotionEffect(new PotionEffect(5, 5000));
+            }
+
+            HandlePotions();
+
+            float moveH = Input.GetAxis("Horizontal");
+            float moveV = Input.GetAxis("Vertical");
+
+            body.velocity = transform.up * moveV * forceMultiplier + transform.right * moveH * forceMultiplier;
         }
-
-        HandlePotions();
-
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
-
-        body.velocity = transform.up * moveV * forceMultiplier + transform.right * moveH * forceMultiplier;
     }
 
     void HandlePotions()
