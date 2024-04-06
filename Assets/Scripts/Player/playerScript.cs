0e1ad3340606fd05d5691c677c1360961d74c481
yLost <fernandopovinsky@hotmail.com>
2024-04-06 17:43:00 -0300

    Fix: scene

--- Assets/Scripts/Player/playerScript.cs
+++ Assets/Scripts/Player/playerScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class playerScript : MonoBehaviour
@@ -7,10 +9,12 @@ public class playerScript : MonoBehaviour
     private Rigidbody2D body;
     private float forceMultiplier = 4;
     private List<PotionEffect> effects;
+    private TextMeshPro potionListText;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.potionListText = GetComponent<TextMeshPro>();
         this.effects = new List<PotionEffect>();
         this.body = GetComponent<Rigidbody2D>();
     }
@@ -20,18 +24,34 @@ public class playerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            AddPotionEffect(new PotionEffect(5, Time.deltaTime + 5));
+            AddPotionEffect(new PotionEffect(5, 5000));
         }
+
+        HandlePotions();
 
         float moveH = Input.GetAxis("Horizontal");
         float moveV = Input.GetAxis("Vertical");
 
-        for (int i = 0; i < effects.Count; i++) {
+        body.velocity = transform.up * moveV * forceMultiplier + transform.right * moveH * forceMultiplier;
+    }
+
+    void HandlePotions()
+    {
+        for (int i = 0; i < effects.Count; i++)
+        {
             PotionEffect effect = effects[i];
 
-        }
+            Debug.Log(effect.expireIn - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            if (effect.expireIn < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+            {
+                effects.RemoveAt(i);
+                i--;
+                forceMultiplier -= effect.speed;
+                continue;
+            }
 
-        body.velocity = transform.up * moveV * forceMultiplier + transform.right * moveH * forceMultiplier;
+            forceMultiplier += effect.speed;
+        }
     }
 
     void AddPotionEffect(PotionEffect potion)
@@ -43,11 +63,11 @@ public class playerScript : MonoBehaviour
 public class PotionEffect
 {
     public int speed;
-    public float expireIn;
+    public long expireIn;
 
-    public PotionEffect(int speed, float expireIn)
+    public PotionEffect(int speed, long expireIn)
     {
         this.speed = speed;
-        this.expireIn = expireIn;
+        this.expireIn = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + expireIn;
     }
 }
