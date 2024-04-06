7905b13cb5d22c6d57d4ee7ef7f7bce3ea5ab331
yLost <fernandopovinsky@hotmail.com>
2024-04-06 17:26:44 -0300

    Prototype player

--- Assets/Scripts/Multiplayer/simpleScript.cs
+++ Assets/Scripts/Multiplayer/simpleScript.cs
@@ -7,12 +7,11 @@ public class simpleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
--- Assets/Scripts/Player/playerScript.cs
+++ Assets/Scripts/Player/playerScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class playerScript : MonoBehaviour
 {
+    private Rigidbody2D body;
+    private float forceMultiplier = 4;
+    private List<PotionEffect> effects;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.effects = new List<PotionEffect>();
+        this.body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AddPotionEffect(new PotionEffect(5, Time.deltaTime + 5));
+        }
+
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        for (int i = 0; i < effects.Count; i++) {
+            PotionEffect effect = effects[i];
+
+        }
+
+        body.velocity = transform.up * moveV * forceMultiplier + transform.right * moveH * forceMultiplier;
+    }
+
+    void AddPotionEffect(PotionEffect potion)
+    {
+        this.effects.Add(potion);
+    }
+}
+
+public class PotionEffect
+{
+    public int speed;
+    public float expireIn;
+
+    public PotionEffect(int speed, float expireIn)
+    {
+        this.speed = speed;
+        this.expireIn = expireIn;
     }
 }
