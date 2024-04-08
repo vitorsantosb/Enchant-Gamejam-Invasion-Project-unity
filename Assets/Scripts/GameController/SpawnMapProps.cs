c21fb2333f827f9feb04c5cbb1c30d91048e4405
Witenberg S R Souza <wittenberg.rs@gmail.com>
2024-04-08 17:16:14 -0400

    Update: changed List to Vector

--- Assets/Scripts/GameController/SpawnMapProps.cs
+++ Assets/Scripts/GameController/SpawnMapProps.cs
@@ -3,7 +3,7 @@ using Photon.Pun;
 using System.Collections.Generic;
 public class SpawnMapProps : MonoBehaviour
 {
-    public List<GameObject> itemsToSpawn;
+    public GameObject[] itemsToSpawn;
     public int numberOfItems = 10;
     public Vector2 spawnArea = new Vector2(10, 10);
 
@@ -13,7 +13,7 @@ public class SpawnMapProps : MonoBehaviour
         {
             Vector2 randomPosition = new Vector2(Random.Range(-spawnArea.x / 2, spawnArea.x / 2), Random.Range(-spawnArea.y / 2, spawnArea.y / 2));
 
-            GameObject itemToSpawn = itemsToSpawn[Random.Range(0, itemsToSpawn.Count)];
+            GameObject itemToSpawn = itemsToSpawn[Random.Range(0, itemsToSpawn.Length)];
             PhotonNetwork.Instantiate(itemToSpawn.name, randomPosition, Quaternion.identity);
 
         }
