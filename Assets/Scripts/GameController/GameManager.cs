fc58b6c7af37c87d8bf070ee0cc2343a16f9e411
yLost <fernandopovinsky@hotmail.com>
2024-04-09 19:47:24 -0300

    Fix: literal character

--- Assets/Scripts/GameController/GameManager.cs
+++ Assets/Scripts/GameController/GameManager.cs
@@ -106,7 +106,7 @@ public class GameManager : EnumManager
     private void WaveCooldownSpawn()
     {
         if (GetWaveState() == WAVE_STATE.GENERATE_NEW_WAVE && !_enableSpawn)
-        {''
+        {
             if (_waveSpawnCooldown > 0)
             {
                 _waveSpawnCooldown -= Time.deltaTime;
